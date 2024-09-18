b3623d91e89ddbcd6d91ffeb731f8932ab836612
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-09-18 12:08:15 +0200

    migraed to advancedmarkers

--- components/SearchMap.vue
+++ components/SearchMap.vue
@@ -4,7 +4,7 @@ import type { Database } from '~/types/database.types'
 type Nanny = Database['public']['Tables']['users_meta']['Row']
 
 const props = defineProps<{
-  center: google.maps.LatLngLiteral,
+  center: google.maps.LatLngLiteral
   nannys: Nanny[]
 }>()
 
@@ -13,7 +13,7 @@ const mapCanvas = ref()
 const locations = props.nannys!.map((a) => {
   return {
     lat: Number(a.location!.split(',')[0].trim()),
-    lng: Number(a.location!.split(',')[1].trim())
+    lng: Number(a.location!.split(',')[1].trim()),
   }
 })
 
@@ -25,56 +25,62 @@ const loadGMaps = async () => {
   const config = useRuntimeConfig()
   const loader = new Loader({
     apiKey: config.public.googleApiKey,
-    libraries: ['places']
+    libraries: ['places'],
   })
-  await loader.load()
 
-  const option:google.maps.MapOptions = {
+  const option: google.maps.MapOptions = {
+    mapId: 'SEARCH_MAP',
     // center: props.center
     // zoom: 12
   }
-  const map = new google.maps.Map(mapCanvas.value, option)
-  const infoWindow = new google.maps.InfoWindow({ maxWidth: 200 })
-  const bounds = new google.maps.LatLngBounds()
+  loader
+    .importLibrary('maps')
+    .then(async ({ Map }) => {
+      const map = new Map(mapCanvas.value, option)
+      // const map = new google.maps.Map(mapCanvas.value, option)
+      const infoWindow = new google.maps.InfoWindow({ maxWidth: 200 })
+      const bounds = new google.maps.LatLngBounds()
 
-  const icon = {
-    url: 'https://img.icons8.com/offices/2x/person-female-skin-type-1-2.png', // url
-    scaledSize: new google.maps.Size(30, 30), // scaled size
-    origin: new google.maps.Point(0, 0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor
-  }
-  const markers:google.maps.Marker[] = []
-  props.nannys.forEach((place, index) => {
-    markers.push(
-      new google.maps.Marker({
-        map,
-        title: place.name,
-        position: locations[index]
-        // icon
+      // const icon = document.createElement('img')
+      // icon.src = 'https://img.icons8.com/offices/2x/person-female-skin-type-1-2.png'
+
+      const { AdvancedMarkerElement } = await loader.importLibrary('marker')
+
+      const markers: google.maps.marker.AdvancedMarkerElement[] = []
+      props.nannys.forEach((place, index) => {
+        markers.push(
+          new AdvancedMarkerElement({
+            map,
+            title: place.name,
+            position: locations[index],
+            // content: icon,
+          })
+        )
+        bounds.extend(locations[index])
       })
-    )
-    bounds.extend(locations[index])
-  })
-  const infoWindowContent = (nanny:Nanny) => {
-    return `
+      const infoWindowContent = (nanny: Nanny) => {
+        return `
     <h3 class="info-window-title">${nanny.name}</h3>
     <div class="info-window-content">${nanny.about}</div>
     <a class="info-window-link" href="/profile/${nanny.id}">More info</a>
     `
-  }
-  markers.forEach((marker, index) => {
-    marker.addListener('click', () => {
-      infoWindow.close()
-      infoWindow.setContent(infoWindowContent(props.nannys[index]))
-      infoWindow.open(marker.getMap(), marker)
+      }
+      markers.forEach((marker, index) => {
+        marker.addListener('click', () => {
+          infoWindow.close()
+          infoWindow.setContent(infoWindowContent(props.nannys[index]))
+          infoWindow.open(marker.map, marker)
+        })
+      })
+      infoWindow.addListener('closeclick', () => {
+        map.fitBounds(bounds)
+      })
+      map.fitBounds(bounds)
     })
-  })
-  infoWindow.addListener('closeclick', () => {
-    map.fitBounds(bounds)
-  })
-  map.fitBounds(bounds)
+    .catch((e) => {
+      console.log(e)
+    })
 }
-
 </script>
 <template>
   <div id="mapCanvas" ref="mapCanvas" class="w-full" />
@@ -84,7 +90,7 @@ const loadGMaps = async () => {
 h3.info-window-title {
   font-weight: 700;
   font-size: 1.1rem;
-  margin-bottom: 0.3rem;;
+  margin-bottom: 0.3rem;
 }
 .info-window-content {
   margin-bottom: 0.2rem;
