8b8c0b9e48fa575beb4118c1474ed91e6e357a40
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-09-26 15:08:34 +0200

    achanged links

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,30 +1,44 @@
 <template>
-  <footer class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 bg-white dark:bg-gray-900">
+  <footer
+    class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 bg-white dark:bg-gray-900"
+  >
     <div class="sm:flex sm:items-center sm:justify-between">
       <NuxtLink to="/" class="flex items-center mb-4 sm:mb-0">
-        <img src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" class="mr-3 h-8" alt="Flowbite Logo">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">nanny+</span>
+        <img
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+          class="mr-3 h-8"
+          alt="Flowbite Logo"
+        />
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          >nanny+</span
+        >
       </NuxtLink>
-      <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+      <ul
+        class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400"
+      >
         <li>
-          <NuxtLink to="/about" class="mr-4 hover:underline md:mr-6 ">
+          <NuxtLink to="/about" class="mr-4 hover:underline md:mr-6">
             About
           </NuxtLink>
         </li>
         <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+          <NuxtLink to="/privacy" class="mr-4 hover:underline md:mr-6">
+            Privacy Policy
+          </NuxtLink>
         </li>
         <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
-        </li>
-        <li>
-          <a href="#" class="hover:underline">Contact</a>
+          <NuxtLink to="/contact" class="mr-4 hover:underline md:mr-6">
+            Contact
+          </NuxtLink>
         </li>
       </ul>
     </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
     <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-      © 2022 <a href="https://flowbite.com/" class="hover:underline">Suppa Nanny™</a>. All Rights Reserved.
+      © 2022
+      <a href="https://flowbite.com/" class="hover:underline">Suppa Nanny™</a>.
+      All Rights Reserved.
     </span>
   </footer>
 </template>
--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -49,7 +49,7 @@ $bus.on('CLOSE_LOGIN_DIALOG', () => { if (theDialog.value) theDialog.value.open 
               Dashboard
             </NuxtLink>
             <NuxtLink to="/about" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
-              Projects
+              About
             </NuxtLink>
           </div>
         </div>
