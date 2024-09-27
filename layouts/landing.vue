06e026e21fa1993df64efe9aec06555288b29f33
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-09-27 09:39:44 +0200

    added landing page layout

---
+++ layouts/landing.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+  <div>
+    <UNotifications />
+    <div class="min-h-full">
+      <TheHeader />
+      <main>
+        <slot />
+      </main>
+      <TheFooter />
+    </div>
+  </div>
+</template>
